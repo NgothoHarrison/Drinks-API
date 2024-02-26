fa681ad486d025e7202f9489fbc3a8e039ffe348
Harrison Hank <codinghank@gmail.com>
2024-02-26 21:42:20 +0300

    POST method

--- drinks/views.py
+++ drinks/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from rest_framework import status
-from rest_framework import Response
+from rest_framework.response import Response
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
@@ -11,7 +11,7 @@ def drink_list(request):
     if request.method == 'GET':
         drinks = Drink.objects.all()
         serializer = DrinkSerializer(drinks, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse({'drinks' :serializer.data}, safe=False)
     if request.method == 'POST':
         serializer = DrinkSerializer(data=request.data)
         if serializer.is_valid():
